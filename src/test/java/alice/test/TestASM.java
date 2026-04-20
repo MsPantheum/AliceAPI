44b755bb0922186b6fa4182e09de56a123264b8e
Alice <reflection3901@proton.me>
2026-04-20 20:08:11 +0800

    meow.

--- src/test/java/alice/test/TestASM.java
+++ src/test/java/alice/test/TestASM.java
@@ -19,19 +19,19 @@ import static alice.util.constants.Constants.*;
 
 public class TestASM {
     @Test
-    @EnabledOnOs(value = {OS.LINUX,OS.WINDOWS})
-    public void test(){
+    @EnabledOnOs(value = {OS.LINUX, OS.WINDOWS})
+    public void test() {
         Init.ensureInit();
-        System.out.println("Pid:"+ProcessUtil.getPID());
+        System.out.println("Pid:" + ProcessUtil.getPID());
         long target = SymbolLookup.lookup(Objects.requireNonNull(FileUtil.search(FileUtil.JAVA_HOME, System.mapLibraryName("jvm"))).toString(), "JVM_Sleep");
-        System.out.println("Target:0x"+Long.toHexString(target));
-        if(!Platform.win32){
-            int success = mprotect.invoke(AddressUtil.align(target), 1,PROT_READ | PROT_WRITE | PROT_EXEC);
+        System.out.println("Target:0x" + Long.toHexString(target));
+        if (!Platform.win32) {
+            int success = mprotect.invoke(AddressUtil.align(target), 1, PROT_READ | PROT_WRITE | PROT_EXEC);
             assert success == 0;
         } else {
-            VirtualProtect.invoke(target,1,0x40,0);
+            VirtualProtect.invoke(target, 1, 0x40, 0);
         }
-        Unsafe.putByte(target,(byte)0xc3);
+        Unsafe.putByte(target, (byte) 0xc3);
         System.out.println("Try to sleep. Current time:" + System.nanoTime());
         try {
             Thread.sleep(Long.MAX_VALUE);
